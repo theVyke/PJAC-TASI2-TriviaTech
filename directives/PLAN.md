79e4569f53ee135c8a2d30c2155481e318923eb0
Matheus Estrela <m.estrela777@outlook.com>
2026-09-07 14:24:38 -0300

    feat: migração para vite e configurações iniciais do axios

---
+++ directives/PLAN.md
@@ -0,0 +1,16 @@
+### PLANO PARA REFATORAÇÃO DO TRIVIATECH
+
+**Implementação API para geração de perguntas/respostas**
+- Inicialmente API RestCoutries
+- Em seguida, Trivia DB + Google Translate
+
+**Boas práticas de configuração de API**
+- Projeto está em html puro + script.js. Sem build
+- Iniciarei os testes com arquivo config.js ignorado pelo git
+- Eventualmente escalarei para bunder vite + arquivo .env (preciso aprender como configurar)
+
+### LOOPING DE DESENVOLVIMENTO
+
+[X] 1. Instalar e configurar o Axios
+[ ] 2. Configurar API
+
--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+import axios from "axios";
+
+const KEY_RESTCOUNTRIES = import.meta.env.VITE_API_KEY_RESTCOUNTRIES;
+
+var paisBuscado = 'Mexico';
+
+async function buscarPaises() {
+  try {
+    const resposta = await axios.get(`https://api.restcountries.com/countries/v5`,{
+        headers: { 'Authorization': `Bearer ${KEY_RESTCOUNTRIES}`  }, 
+        params: { q: paisBuscado, response_fields: "names.common,capitals,population"},
+    });
+    return resposta.data;
+  } catch (error) {
+    console.error("Erro ao buscar países:", error);
+    return [];
+  }
+}
+
+buscarPaises().then(paises => console.log(paises));
+
 const options = document.querySelectorAll(".option");
 
 function handleAnswer(event) {
